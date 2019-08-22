1fb3ff56d277f7b48ce4b02ac35a0327655d4e2b
Colin Brayton <gringolalia@gmail.com>
2019-08-22 14:10:13 -0300

    init gh-pages

--- app/src/core/core.contants.js
+++
@@ -1,9 +0,0 @@
-(function(){
-
-  'use strict';
-
-  angular.module('app.core')
-    // .constant('API_URL','http://localhost:3000');
-    .constant('API_URL','https://user-crud-test.herokuapp.com');
-
-}());
